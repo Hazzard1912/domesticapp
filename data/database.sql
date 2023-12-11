b76c9ca105e4bed3968d7e68fd2441aa3af0fce5
AngieJoya99 <angie.joya@correounivalle.edu.co>
2023-12-10 20:56:48 -0500

    base de datos

--- data/database.sql
+++ data/database.sql
@@ -1,1 +1,71 @@
+CREATE TABLE usuario(
+    idUsuario SERIAL PRIMARY KEY NOT NULL,
+    contraseña VARCHAR(30),
+    telefono VARCHAR(20),
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    fechaNacimiento DATE,
+    recibo VARCHAR(100),
+    direccion VARCHAR(20),
+    email VARCHAR(50),
+    fotoPerfil VARCHAR(100),
+    fotoDocumento VARCHAR(100)
+);
 
+CREATE TABLE profesional(
+    idProfesional SERIAL PRIMARY KEY,
+    codigoUsuario INT,
+    imagenID VARCHAR(100),
+    disponibilidad BOOLEAN,
+    FOREIGN KEY(codigoUsuario) REFERENCES usuario(idUsuario)
+);
+
+CREATE TABLE categoria(
+    idCategoria SERIAL PRIMARY KEY,
+    nombre VARCHAR(30),
+    unidadTrabajo VARCHAR(30)
+);
+
+CREATE TABLE servicio(
+    idServicio SERIAL PRIMARY KEY,
+    codigoCategoria INT,
+    descripcion TEXT,
+    labor VARCHAR(50),
+    FOREIGN KEY (codigoCategoria) REFERENCES categoria(idCategoria)
+);
+
+CREATE TABLE medioPago(
+    idMedioPago SERIAL PRIMARY KEY,
+    nombre VARCHAR(30),
+    numero VARCHAR(30),
+    banco VARCHAR(30)
+);
+
+CREATE TABLE usuarioPago(
+    codigoUsuario INT,
+    codigoMedioPago INT,
+    FOREIGN KEY (codigoUsuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (codigoMedioPago) REFERENCES medioPago(idMedioPago),
+    PRIMARY KEY (codigoUsuario, codigoMedioPago)
+);
+
+CREATE TABLE profesionalServicio(
+    codigoProfesional INT,
+    codigoServicio INT,
+    precio INT,
+    FOREIGN KEY (codigoProfesional) REFERENCES profesional(idProfesional),
+    FOREIGN KEY (codigoServicio) REFERENCES servicio(idServicio),
+    PRIMARY KEY (codigoProfesional, codigoServicio)
+);
+
+CREATE TABLE usuarioProfesionalServicio(
+    codigoProfesional INT,
+    codigoUsuario INT,
+    codigoServicio INT,
+    calificacion INT,
+    pago INT,
+    FOREIGN KEY (codigoProfesional) REFERENCES profesional(idProfesional),
+    FOREIGN KEY (codigoUsuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (codigoServicio) REFERENCES servicio(idServicio),
+    PRIMARY KEY (codigoProfesional, codigoUsuario, codigoServicio)
+);
---
+++ data/records.sql
@@ -0,0 +1,57 @@
+INSERT INTO usuario(contraseña,telefono,nombre,apellido,fechaNacimiento,direccion,
+email,fotoPerfil,fotoDocumento)
+VALUES
+('contraseña1','1-414-275-2817','Mannix','Montgomery', '1995-10-01', '3.425071160714475, -76.52570131384293', 'sed.dictum@yahoo.net','http://example.com/profile-pic-1.jpg', 'http://example.com/profile-pic-2.jpg'),
+('contraseña2', '(493) 353-4031','"Rowan','Carver','1999-06-29', '3.4322680330963444, -76.48072603399139', 'sem@google.edu','http://example.com/profile-pic-3.jpg', 'http://example.com/profile-pic-4.jpg' ),
+('contraseña3', '1-970-358-7271', 'Angela', 'Norris', '1995-05-01', '3.385487397751655, -76.51814821377114', 'suscipit.nonummy@yahoo.edu', 'http://example.com/profile-pic-5.jpg', 'http://example.com/profile-pic-6.jpg' ),
+('contraseña4', '(129) 646-3008','Alexis', 'Downs', '2001-12-06', '3.2586714699746753, -76.5375459488978', 'urna.nullam@hotmail.couk','http://example.com/profile-pic-7.jpg', 'http://example.com/profile-pic-8.jpg' );
+
+INSERT INTO profesional(codigoUsuario,imagenID,disponibilidad)
+VALUES
+(1 ,'http://example.com/profile-pic-12.jpg','true'),
+(2 ,'http://example.com/profile-pic-10.jpg', 'true'),
+(3 ,'http://example.com/profile-pic-11.jpg', 'false'),
+(4,'http://example.com/profile-pic-13.jpg', 'false');
+
+INSERT INTO categoria(nombre, unidadTrabajo)
+VALUES
+('categoria1', 'hora'),
+('categoria2', 'hora'),
+('categoria3', 'hora'),
+('categoria4', 'm2');
+
+INSERT INTO servicio(codigoCategoria, descripcion, labor)
+VALUES
+(1 , 'Descripcion del servicio1' , 'labor1' ),
+(1 ,'Descripcion del servicio2' , 'labor2' ),
+(1 , 'Descripcion del servicio3' , 'labor3'),
+(2 , 'Descripcion del servicio4', 'labor4');
+
+
+INSERT INTO medioPago(nombre, numero, banco)
+VALUES
+('Nequi', '00001' , 'BBVA'),
+('Tarjeta de crédito', '00002' , 'AV Villas'),
+('Daviplata', '00003' , 'Davivienda'),
+('Paypal', '00004' , 'Banco popular de Colombia');
+
+INSERT INTO usuarioPago(CodigoUsuario,codigoMedioPago)
+VALUES
+(1 , 1),
+(2 , 2),
+(3 , 3),
+(4 , 4);
+
+INSERT INTO profesionalServicio(codigoProfesional,CodigoServicio,precio)
+VALUES
+(1 ,2 , 100000),
+(1 ,3 , 60000),
+(2,1 , 170000),
+(4 ,4 , 300000);
+
+INSERT INTO usuarioProfesionalServicio(codigoProfesional,codigoUsuario, codigoServicio, calificacion, pago)
+VALUES
+(1 ,1 ,1 , 5 ,100000),
+(2 ,2 ,2 , 2 ,60000),
+(3 ,3 ,3 , 4 ,170000),
+(4 ,4 ,4 , 5 ,300000);
